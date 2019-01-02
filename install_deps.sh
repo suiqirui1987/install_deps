b8e39ea4b09fd0835106d175d9c408c7aa477411
孙其瑞 <suiqirui1987@qq.com>
2019-01-02 18:53:29 +0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -14,7 +14,8 @@ sudo apt-get  -f -y install libomp-dev
 sudo apt-get -f -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 sudo apt-get -f -y install libeigen3-dev
 sudo apt-get -f -y install libjpeg62-dev 
-sudo apt-get -f -y install libtiff5-dev libjasper-dev
+sudo apt-get -f -y install libtiff-dev
+sudo apt-get -f -y install libtiff5-dev
 sudo apt-get -f -y install libpng-dev
 sudo apt-get -f -y install libjsoncpp-dev
 sudo apt-get -f -y install libboost-all-dev
@@ -25,7 +26,7 @@ cd flann-1.8.5
 mkdir build 
 cd build
 cmake ..
-make  -j8                                                              
+make  -j18                                                              
 sudo make install
 
 wget http://www.vtk.org/files/release/8.0/VTK-8.0.1.tar.gz
@@ -34,7 +35,7 @@ cd VTK-8.0.1
 mkdir build 
 cd build
 cmake ..
-make  -j8                                                              
+make  -j18                                                              
 sudo make install
 
 cd ../../
@@ -46,7 +47,7 @@ cd pcl-pcl-1.8.1
 mkdir build 
 cd build
 cmake ..
-make -j8
+make -j18
 sudo make install
 
 cd ../../
@@ -59,14 +60,12 @@ cd opencv-3.3.1
 mkdir build
 cd build
 cmake -D BUILD_NEW_PYTHON_SUPPORT=OFF -D WITH_OPENCL=OFF -D WITH_OPENMP=ON -D INSTALL_C_EXAMPLES=OFF -D BUILD_DOCS=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=OFF -D WITH_OPENGL=OFF -D WITH_VTK=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D WITH_CUDA=OFF -D BUILD_opencv_gpu=OFF ..
-make -j8
-sudo make install
-echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf.d/opencv.conf
-sudo ldconfig
-echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig" | sudo tee -a /etc/bash.bashrc
-echo "export PKG_CONFIG_PATH" | sudo tee -a /etc/bash.bashrc
-source /etc/bash.bashrc
-cd ../..
+make -j18
+make install
+
+
+
+cd ../../
 
 
 
